d03fc967a0f2025722cfddf76fa83ff7bcc21e55
charles <charles@charless-MacBook-Pro.local>
2017-01-29 17:01:43 -0600

    added delete check functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,10 +2,29 @@ $(function(){
 getTasks();
 console.log('inside jquery DOM ready function');
 $('#addtask').on('click', registerTask);
-$('#taskList').on('click','.delete',deleteTask);
+$('#taskList').on('click','.delete',deleteCheck);
 $('#taskList').on('click','.complete',completeTask);
+$('#taskList').on('click','.yes',deleteTask);
+$('#taskList').on('click','.no',hideDeleteCheck);
 
 });
+
+
+function hideDeleteCheck(event){
+  console.log('inside hideDeleteCheck function');
+  event.preventDefault();
+   var taskId = $(this).attr('id')
+   var temp='#'+taskId+'.deletetask';
+  $(temp).hide();
+}
+
+function deleteCheck(event){
+  console.log('inside deleteCheck function');
+  event.preventDefault();
+   var taskId = $(this).attr('id')
+   var temp='#'+taskId+'.deletetask';
+  $(temp).show();
+}
 
 
 function completeTask(event){
@@ -57,27 +76,47 @@ function getTasks(list){
   });
 }
 
+// function displayTasks(tasklist){
+//   console.log('inside function displayTasks',tasklist);
+//   $('#taskList').empty();
+//   tasklist.forEach(function(obj){
+//     var $div = $('<div class="table-row"></div>');
+//     var $form = $('<form></form>');
+//     $form.append('<div class="table-cell"><span>'+obj.task+'</span></div>');
+//     if(obj.is_complete){
+//       $form.append('<div class="table-cell"><button id="'+obj.id+'" class="completedTask btn-success">Task Completed</button></div>');
+//     } else {
+//     $form.append('<div class="table-cell"><button id="'+obj.id+'" class="complete btn-info">Complete</button></div>');
+//     }
+//     $form.append('<div class="table-cell"><button id="'+obj.id+'" class="delete btn-info">Delete</button></div>');
+//     $div.append($form);
+//  $('#taskList').append($div);
+//
+//   });
+//
+// }
+
 function displayTasks(tasklist){
-  // <div class="table-row">
-  //     <div class="table-head">Table Header</div>
-  //     <div class="table-head">Table Header</div>
-  //     <div class="table-head">Table Header</div>
-  //   </div>
   console.log('inside function displayTasks',tasklist);
   $('#taskList').empty();
   tasklist.forEach(function(obj){
-    var $div = $('<div class="table-row"></div>');
+    var $div = $('<div class="row "></div>');
     var $form = $('<form></form>');
-    $form.append('<div class="table-cell"><span>'+obj.task+'</span></div>');
+    $form.append('<div class="col-md-3"><span>'+obj.task+'</span></div>');
     if(obj.is_complete){
-      $form.append('<div class="table-cell"><button id="'+obj.id+'" class="completedTask">Task Completed</button></div>');
+      $form.append('<div class="col-md-2"> <button class="btn-success"><span class="glyphicon glyphicon-ok"></span>Completed</button></div>');
     } else {
-    $form.append('<div class="table-cell"><button id="'+obj.id+'" class="complete">Complete</button></div>');
+    $form.append('<div class="col-md-2"><button id="'+obj.id+'" class="complete btn-info">Complete</button></div>');
     }
-    $form.append('<div class="table-cell"><button id="'+obj.id+'" class="delete">Delete</button></div>');
+    $form.append('<div class="col-md-2"><button id="'+obj.id+'" class="delete btn-info">Delete</button></div>');
+    //delete check start
+     $form.append('<div id="'+obj.id+'" class="deletetask col-md-1"><span>are you sure?</span></div>');
+     $form.append('<div id="'+obj.id+'" class="deletetask col-md-1"><button id="'+obj.id+'" class="yes btn-info">Yes</button></div>');
+     $form.append('<div id="'+obj.id+'" class="deletetask col-md-1"><button id="'+obj.id+'" class="no btn-info">No</button></div>');
+
+    //delete check end
     $div.append($form);
  $('#taskList').append($div);
-
   });
 
 }
